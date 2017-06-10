28e8af5f039800dd9c17bcfb754b232df2c1770f
Swend5 <svend5@hotmail.com>
2017-06-10 14:12:52 +0200

    -

--- install.lua
+++ install.lua
@@ -1,17 +1,1 @@
-ghstr = "https://raw.githubusercontent.com/Swend5/ccstuff/master/%s.lua %s"
-programs = {
-    "mine",
-    "api",
-    "refuel",
-    "pmine",
-    "vgps",
-    "build",
-    "setupgps",
-    "lumberjack",
-    "queen",
-}
-
-for _, program in ipairs(programs) do
-    shell.run("delete", program)
-    shell.run("wget", string.format(ghstr, program, program))
-end
+wat

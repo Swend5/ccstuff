304eb5c745fe83ff8dbe19aba70b876f1dd72441
Swend5 <svend5@hotmail.com>
2017-06-10 12:54:56 +0200

    -

--- refuel.lua
+++ refuel.lua
@@ -1,14 +1,15 @@
 lastFuel = -1
+print("Current fuel level: " .. turtle.getFuelLevel())
 while true do
   for i=1, 16 do
     turtle.select(i)
     turtle.refuel()
   end
   fuel = turtle.getFuelLevel()
-  print(turtle.getFuelLevel())
   if fuel == lastFuel then
     break
   end
   lastFuel = fuel
   os.sleep(1)
 end
+print("Refuel complete. Fuel level: " .. turtle.getFuelLevel())
